f74331673224492d149b612c2e9114678fa929fd
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-03-11 14:43:52 -0300

    initial commit

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.JWT_SECRET as string,
+}
+
+export default NextAuth(authOptions)
