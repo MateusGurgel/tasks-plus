db821af0c7b6e0ce653dc4830cee0f1a65ffffa7
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-03-11 12:56:22 -0300

    image added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,30 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
+import styles from '../styles/Home.module.css'
+import Image from 'next/image'
 
-const inter = Inter({ subsets: ['latin'] })
+import heroImage from "../../public/assets/hero.png"
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <title>Tasks+</title>
       </Head>
 
-      <h1>My project</h1>
-    </>
+      <main className={styles.main}>
+        <div className={styles.logoContent}>
+          <Image
+            className={styles.hero}
+            alt="Logo Tasks+"
+            src={heroImage}
+            priority
+          />
+        </div>
+        <h1 className={styles.title}>Let's organize your tasks!</h1>
+
+        
+      </main>
+
+    </div>
   )
 }
