c4aaf5963db878321f5e6127ff167440fab40efc
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-03-12 10:29:12 -0300

    Textarea added

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,34 +1,53 @@
-import styles from "./styles.module.css"
-import Head from 'next/head'
-import { GetServerSideProps } from "next"
-import { getSession } from "next-auth/react"
+import { GetServerSideProps } from "next";
+import styles from "./styles.module.css";
+import Head from "next/head";
 
-export default function Dashboard(){
-    return(
+import { getSession } from "next-auth/react";
+import { Textarea } from "../../components/textArea";
+
+export default function Dashboard() {
+  return (
     <div className={styles.container}>
-        <Head>
-            <title>Dashboard</title>
-        </Head>
+      <Head>
+        <title>Meu painel de tarefas</title>
+      </Head>
 
-        <h1>Dashboard</h1>
+      <main className={styles.main}>
+        <section className={styles.content}>
+          <div className={styles.contentForm}>
+            <h1 className={styles.title}>Qual sua tarefa?</h1>
+
+            <form>
+              <Textarea placeholder="Digite qual sua tarefa..." />
+              <div className={styles.checkboxArea}>
+                <input type="checkbox" className={styles.checkbox} />
+                <label>Deixar tarefa publica?</label>
+              </div>
+
+              <button className={styles.button} type="submit">
+                Registrar
+              </button>
+            </form>
+          </div>
+        </section>
+      </main>
     </div>
-    )
+  );
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
 
-    const session = await getSession({ req });
-
-    if (!session?.user){
-        return{
-            redirect:{
-                destination: '/',
-                permanent: false
-            }
-        }
-    }
-    
+  if (!session?.user) {
     return {
-        props: {},
-    }
-}
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
