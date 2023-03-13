820dc65d1b544b0a623364c5fcc846e8e8734b3f
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-03-12 22:52:47 -0300

    comments added

--- src/pages/task/[id].tsx
+++ src/pages/task/[id].tsx
@@ -1,23 +1,46 @@
+import { Textarea } from "@/components/textArea";
 import { db } from "@/services/firebaseConnection";
-import { async } from "@firebase/util";
 import { doc, getDoc } from "firebase/firestore";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
+import styles from "../task/styles.module.css"
 
+interface TaskProps {
+    item: {
+      task: string;
+      created: string;
+      public: boolean;
+      user: string;
+      taskId: string;
+    };
+  }
+  
 
-export default function Task(){
-    return(
-        <div>
+export default function Task({ item }: TaskProps) {
+    return (
+      <div className={styles.container}>
         <Head>
-            <title>Task details</title>
+          <title>Tarefa - Detalhes da tarefa</title>
         </Head>
-
-        <main>
-            <h1>Tasks</h1>
+  
+        <main className={styles.main}>
+          <h1>Tarefa</h1>
+          <article className={styles.task}>
+            <p>{item.task}</p>
+          </article>
         </main>
-    </div>
-    )
-}
+  
+        <section className={styles.commentsContainer}>
+          <h2>Deixar comentário</h2>
+  
+          <form>
+            <Textarea placeholder="Digite seu comentário..." />
+            <button className={styles.button}>Enviar comentário</button>
+          </form>
+        </section>
+      </div>
+    );
+  }
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     const id = params?.id as string;
@@ -57,6 +80,8 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     }
 
     return{
-        props: {}
+        props: {
+            item: task
+        }
     }
 }
